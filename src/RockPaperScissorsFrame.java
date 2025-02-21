16a6314103c623c7b47a498d2d076b23712f3d15
mabreydn <mabreydn@mail.uc.edu>
2025-02-21 09:43:39 -0500

    Inital Commit
    Creation of project, creation of java classes for assignment with some code inserted already.

---
+++ src/RockPaperScissorsFrame.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RockPaperScissorsFrame extends JFrame
+{
+    JPanel mainPanel;
+    JPanel topPanel;
+    JPanel middlePanel;
+    JPanel bottomPanel;
+
+    JButton rock;
+    JButton paper;
+    JButton scissors;
+    JButton exit;
+
+    Font titleFont = new Font("Arial", Font.BOLD, 20);
+    Font buttonFont = new Font("Courier New", Font.ITALIC, 15);
+    Font displayFont = new Font("Times New Roman", Font.PLAIN, 15);
+}
